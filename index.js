9b5f3b760f5ed8474f0539d09d2de6f19cf95398
ArifR_ <arifr0751@gmail.com>
2024-07-29 22:21:55 +0700

    the new one

---
+++ index.js
@@ -0,0 +1,9 @@
+const input = document.getElementById("id").value
+const btn = document.getElementById("id")
+
+btn.addEventListener("click",()=>{
+    const url = `https://api.jikan.moe/v4/anime?q=${input}&sfw`
+    async myFun(){
+        
+    }
+})
