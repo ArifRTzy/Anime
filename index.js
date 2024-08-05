49afb0654d0d25e95df26f326290862b551d183b
ArifR_ <arifr0751@gmail.com>
2024-08-05 19:50:29 +0700

    final

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const btn = document.getElementById("btn");
 const displays = document.getElementById("display");
 
-document.getElementById("input").addEventListener("keypress",(event)=>{
-    if (event.key === "Enter"){
-        event.preventDefault()
+document.getElementById("input").addEventListener("keypress", event => {
+  if (event.key === "Enter") {
+    event.preventDefault();
 
-        btn.click()
-    }
-})
+    btn.click();
+  }
+});
 
 btn.addEventListener("click", () => {
-  const input = document.getElementById("input").value; 
-  const url = `https://api.jikan.moe/v4/anime?q=${input}&sfw`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-      data.data.forEach((item) => {
-        const anime = item;
-        const imageUrl = anime.images.jpg.image_url;
-        const animeDataDiv = document.createElement("div");
-        animeDataDiv.innerHTML = `
-          <p><b>Title:</b> ${anime.title}</p>
-          <img src="${imageUrl}" alt="${anime.title}">
-          <p><b>Synopsis:</b> ${anime.synopsis}</p>
-          <p><b>Type:</b> ${anime.type}</p>
-          <p><b>Total Episodes:</b> ${anime.episodes}</p>
-        `;
-        displays.appendChild(animeDataDiv);
-      });
-    })
-    .catch((error) => console.error("Error fetching data:", error));
+  const input = document.getElementById("input").value.trim();
+  if (input === "") {
+    console.log("empty");
+  } else {
+    const url = `https://api.jikan.moe/v4/anime?q=${input}&sfw`;
+    displays.innerHTML = "";
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        data.data.forEach(item => {
+          const anime = item;
+          const imageUrl =
+            anime.images.jpg.image_url || anime.images.jpg.large_image_url;
+          const animeUrl = document.createElement("a");
+          animeUrl.href = anime.url;
+          animeUrl.innerHTML = `
+          <div>
+            <img src="${imageUrl}" alt="${anime.title}">
+            <p>${anime.title}</p>
+          </div>
+          `;
+          displays.appendChild(animeUrl);
+        });
+      })
+      .catch(error => console.error("Error fetching data:", error));
+  }
 });
